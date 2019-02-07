da5ca7f65ef8cd0c7d580e0f4197b13db8b17841
Andrew Fernandez <fdez26@ymail.com>
2019-02-06 22:04:48 -0500

    main for infixtoPostfix test

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ int main() {
     const std::string prefix1 = "^ / x y * a b";
     const std::string postfix1 = "X Y + A B + *";
     const std::string postfix2 = "V C +";
+    const std::string infix2 = "((X + B) * (Y - D))";
     NotationConverter n4;
     
     // std::cout << n4.prefixToInfix(prefix1) << std::endl;
     std::cout << n4.postfixToInfix(postfix1) << std::endl;
-
+    std::cout << n4.infixToPostfix(infix2) << std::endl;
     return 0;
 }
