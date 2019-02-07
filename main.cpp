544df7552c46d523c5c101605a1e0506a50e2b23
Andrew Fernandez <fdez26@ymail.com>
2019-02-06 19:11:48 -0500

    fix syntax error

--- main.cpp
+++ main.cpp
@@ -10,10 +10,6 @@ int main() {
     const std::string infix2 = "(A *     b)";
     const std::string prefix1 = "+ / * x y g h";
     NotationConverter nc, nc2, nc3, n4;
-
-    nc.decodeInput(infix1);
-    nc2.decodeInput(infix1);
-    nc3.decodeInput(infix2);
     auto deque = nc.getDeque();
     auto deque2 = nc2.getDeque();
     auto deque3 = nc3.getDeque();
