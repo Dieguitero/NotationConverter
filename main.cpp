9c51e2b47f0423a516bb0d2b9a5828c83eeb0e0a
Andrew Fernandez <fdez26@ymail.com>
2019-02-06 19:15:29 -0500

    syntax error

--- main.cpp
+++ main.cpp
@@ -5,14 +5,7 @@
 #include <iostream>
 
 int main() {
-
-    const std::string infix1 = "(A + B) ";
-    const std::string infix2 = "(A *     b)";
-    const std::string prefix1 = "+ / * x y g h";
-    NotationConverter nc, nc2, nc3, n4;
-    auto deque = nc.getDeque();
-    auto deque2 = nc2.getDeque();
-    auto deque3 = nc3.getDeque();
+    NotationConverter n4;
     
     std::cout << n4.prefixToInfix(prefix1) << std::endl;
 
